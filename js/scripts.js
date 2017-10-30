17efcd9390f597a0fe89aa4b48f49d85c0ee38a8
Hubert Maćkowiak <macowiak.h@wp.pl>
2017-10-30 23:43:45 +0100

    new file script.js moudle_8.5

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt("Eneter long a of Triangle");
+	h = prompt("Eneter long h of Triangle");
+	triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
