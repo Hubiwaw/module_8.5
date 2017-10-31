8a1168eb6eafb9249349fb3c3da2fa1f6c6b8e86
Hubert Maćkowiak <macowiak.h@wp.pl>
2017-10-31 09:57:23 +0100

    added corrections to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var a = prompt("Eneter long a of Triangle");
-	h = prompt("Eneter long h of Triangle");
-	triangleArea = a*h/2;
+var	h = prompt("Eneter long h of Triangle");
+var	triangleArea = a*h/2;
 alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
